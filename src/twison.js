36d84831afc5ed308dbce0fa10deb086291930f3
Mike Walker <michael@lazerwalker.com>
2015-11-18 19:04:07 -0500

    Separate Twison functions from on-load auto-convert behavior

--- src/twison.js
+++ src/twison.js
@@ -1,81 +1,78 @@
-window.onload = function() {
-	window.Twison = {
-		extractLinksFromText: function(text) {
-	    var links = text.match(/\[\[.+?\]\]/g)
-	    if (links) {
-	      return links.map(function(link) {
-	        var differentName = link.match(/\[\[(.*?)\-\&gt;(.*?)\]\]/);
-	        if (differentName) {
-	          // [[name->link]]
-	          return {
-	            name: differentName[1],
-	            link: differentName[2]
-	          };
-	        } else {
-	          // [[link]]
-	          link = link.substring(2, link.length-2)
-	          return {
-	            name: link,
-	            link: link
-	          }
-	        }
-	      });
-	    }
-	  },
-
-  	convertPassage: function(passage) {
-	  	var dict = {text: passage.innerHTML};
-
-      var links = Twison.extractLinksFromText(dict.text);
-      if (links) {
-        dict.links = links;
-      }
-
-      ["name", "pid", "position", "tags"].forEach(function(attr) {
-        var value = passage.attributes[attr].value;
-        if (value) {
-          dict[attr] = value;
+window.Twison = {
+	extractLinksFromText: function(text) {
+    var links = text.match(/\[\[.+?\]\]/g)
+    if (links) {
+      return links.map(function(link) {
+        var differentName = link.match(/\[\[(.*?)\-\&gt;(.*?)\]\]/);
+        if (differentName) {
+          // [[name->link]]
+          return {
+            name: differentName[1],
+            link: differentName[2]
+          };
+        } else {
+          // [[link]]
+          link = link.substring(2, link.length-2)
+          return {
+            name: link,
+            link: link
+          }
         }
       });
-
-      if(dict.position) {
-        var position = dict.position.split(',')
-        dict.position = {
-          x: position[0],
-          y: position[1]
-        }
-      }
-
-      if (dict.tags) {
-        dict.tags = dict.tags.split(" ");
-      }
-
-      return dict;
-		},
-
-    convertStory: function(story) {
-      var passages = story.getElementsByTagName("tw-passagedata");
-      var convertedPassages = Array.prototype.slice.call(passages).map(Twison.convertPassage);
-
-      var dict = {
-        passages: convertedPassages
-      };
-
-      ["name", "startnode", "creator", "creator-version", "ifid"].forEach(function(attr) {
-        var value = story.attributes[attr].value;
-        if (value) {
-          dict[attr] = value;
-        }
-      });
-
-      return dict;
-    },
-
-		convert: function() {
-      var story = document.getElementsByTagName("tw-storydata")[0];
-      var convertedStory = Twison.convertStory(story);
-      return JSON.stringify(convertedStory, null, 2);
     }
+  },
+
+	convertPassage: function(passage) {
+  	var dict = {text: passage.innerHTML};
+
+    var links = Twison.extractLinksFromText(dict.text);
+    if (links) {
+      dict.links = links;
+    }
+
+    ["name", "pid", "position", "tags"].forEach(function(attr) {
+      var value = passage.attributes[attr].value;
+      if (value) {
+        dict[attr] = value;
+      }
+    });
+
+    if(dict.position) {
+      var position = dict.position.split(',')
+      dict.position = {
+        x: position[0],
+        y: position[1]
+      }
+    }
+
+    if (dict.tags) {
+      dict.tags = dict.tags.split(" ");
+    }
+
+    return dict;
+	},
+
+  convertStory: function(story) {
+    var passages = story.getElementsByTagName("tw-passagedata");
+    var convertedPassages = Array.prototype.slice.call(passages).map(Twison.convertPassage);
+
+    var dict = {
+      passages: convertedPassages
+    };
+
+    ["name", "startnode", "creator", "creator-version", "ifid"].forEach(function(attr) {
+      var value = story.attributes[attr].value;
+      if (value) {
+        dict[attr] = value;
+      }
+    });
+
+    return dict;
+  },
+
+	convert: function() {
+    var story = document.getElementsByTagName("tw-storydata")[0];
+    var convertedStory = Twison.convertStory(story);
+    return JSON.stringify(convertedStory, null, 2);
   }
-  document.getElementById("output").innerHTML = Twison.convert()
-}	
+}
